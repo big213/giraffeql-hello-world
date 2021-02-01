89827851ca8067b221067534329c00805764c598
James Chang <jc4095@nyu.edu>
2021-02-01 12:09:35 -0500

    Upgrade jomql to 0.3.3

--- schema.js
+++ schema.js
@@ -1,13 +1,12 @@
 // register the root resolver
 
-const { JomqlRootResolverType, BaseScalars, lookupSymbol } = require("jomql");
+const { JomqlRootResolverType, BaseScalars } = require("jomql");
 
 module.exports = new JomqlRootResolverType({
   name: "getHelloWorld",
   restOptions: {
     method: "get",
     route: "/hello_world",
-    query: lookupSymbol,
   },
   allowNull: false,
   type: BaseScalars.string,
